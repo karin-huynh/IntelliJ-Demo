e84d7d4c72e938acc945d2fee2fde4fb914474a2
Karin Huynh <huynhsuan29@gmail.com>
2025-10-31 19:21:16 -0500

    New Commit

--- src/main/java/edu/utsa/cs3443/demo1/Testing.java
+++ src/main/java/edu/utsa/cs3443/demo1/Testing.java
@@ -1,5 +1,7 @@
 package edu.utsa.cs3443.demo1;
 
 public class Testing {
-    System.out.println("Hi, this is Karin");
+    public static void main(String[] args) {
+        System.out.println("Hello, this is Karin from Application Programming");
+    }
 }
